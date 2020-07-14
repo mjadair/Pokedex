aa43842eec43175a9ee090c1633b6aeb144e3452
Michael Adair <mjadair91@gmail.com>
2020-07-14 18:46:16 +0100

    inherited search bar delegate and comments

--- Pokedex/PokemonListViewController.swift
+++ Pokedex/PokemonListViewController.swift
@@ -8,25 +8,44 @@
 
 import UIKit
 
-class PokemonListViewController: UITableViewController {
-    var pokemon: [PokemonListResult] = []
-        @IBOutlet var searchBar: UILabel!
+// This is the list view controller on the front page
+// It inherits from UITableViewController and UISearchBarDelegate
+class PokemonListViewController: UITableViewController, UISearchBarDelegate {
     
+    // This is our Pokemon, returned from the results - from the Pokemon Models.
+    var pokemon: [PokemonListResult] = []
+        
+    // Creates an outlet for our Searchbar to link this controller to the storyboard
+    @IBOutlet var searchBar: UISearchBar!
+    
+    
+    // A function to capitalise the first letter of words returned from the API
     func capitalise(text: String) -> String {
         return text.prefix(1).uppercased() + text.dropFirst()
     }
     
+    
+    // We take Swift's viewDidLoad function and override it to pass new arguments to it
     override func viewDidLoad() {
+        
+        // We call the superClass for viewDidLoad
         super.viewDidLoad()
         
+        
+        // guard allows us to handle url even if it is null
         guard let url = URL(string: "https://pokeapi.co/api/v2/pokemon?limit=151") else {
             return
         }
         
+        // we use the URL to fetch
+        // dataTask is the method that fetches
+        // we then set the data from the response
+        // this method needs to be concluded with the .resume() method.
         URLSession.shared.dataTask(with: url) { (data, response, error) in
             guard let data = data else {
                 return
             }
+            
             
             do {
                 let entries = try JSONDecoder().decode(PokemonListResults.self, from: data)
