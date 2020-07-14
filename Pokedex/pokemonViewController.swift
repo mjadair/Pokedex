24862300386466f7ae17c2a68297b18a4ee23700
Michael Adair <mjadair91@gmail.com>
2020-07-14 14:43:08 +0100

    added pokemon views

---
+++ Pokedex/pokemonViewController.swift
@@ -0,0 +1,24 @@
+//
+//  pokemonViewController.swift
+//  Pokedex
+//
+//  Created by Michael Adair on 14/07/2020.
+//  Copyright © 2020 Michael Adair. All rights reserved.
+//
+
+import UIKit
+
+
+class PokemonViewController: UIViewController {
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var numberLabel: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    override func viewDidLoad() {
+        super .viewDidLoad()
+        
+        nameLabel.text = pokemon.name
+        nameLabel.text = String(pokemon.number)
+    }
+}
